fdd769e2a072e4979efb3fd51a6fd154a6bc81bd
Shakhriyor Kadamboev <shakhriyor.kadamboev@student.uni-halle.de>
2026-03-12 15:44:32 +0100

    Add Vercel Analytics to the layout and update package dependencies
    - Integrated Vercel Analytics in the RootLayout component of layout.tsx.
    - Added @vercel/analytics dependency in package.json and package-lock.json.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
@@ -45,6 +46,7 @@ export default function RootLayout({
             {children}
           </AuthProvider>
         </Providers>
+        <Analytics/>
       </body>
     </html>
   );
